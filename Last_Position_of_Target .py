906bb22db7c824077f7a5a61ad5175a02c9a70e2
Chih-Chao Chang <cchan113@jhu.edu>
2017-08-06 23:09:51 -0400

    Create Last_Position_of_Target .py

---
+++ Last_Position_of_Target .py
@@ -0,0 +1,39 @@
+'''
+Last Position of Target
+Find the last position of a target number in a sorted array. Return -1 if target does not exist.
+
+Example
+Given [1, 2, 2, 4, 5, 5].
+
+For target = 2, return 2.
+
+For target = 5, return 5.
+
+For target = 6, return -1.
+'''
+class Solution:
+    # @param {int[]} A an integer array sorted in ascending order
+    # @param {int} target an integer
+    # @return {int} an integer
+    def lastPosition(self, A, target):
+        # Write your code here
+        if not A or target is None:
+            #not A means A should not be [], or None
+            return -1
+        
+        start, end= 0, len(A)-1
+        while start+1 < end:
+            mid = start + (end-start)/2
+            if A[mid] < target:
+                start = mid
+            elif A[mid] > target:
+                end =mid
+            else:
+                start = mid
+        
+        if A[end]== target:
+            return end
+        elif A[start] == target:
+            return start
+        else:
+            return -1
