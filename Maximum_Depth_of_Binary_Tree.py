64e16ef7664bc954b2a5cc9d7c5db7fb0946dcdd
Chih-Chao Chang <cchan113@jhu.edu>
2017-09-17 22:39:48 -0400

    Create Maximum_Depth_of_Binary_Tree.py

---
+++ Maximum_Depth_of_Binary_Tree.py
@@ -0,0 +1,34 @@
+'''
+Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Example
+Given a binary tree as follow:
+
+  1
+ / \ 
+2   3
+   / \
+  4   5
+The maximum depth is 3.
+
+'''
+
+"""
+Definition of TreeNode:
+class TreeNode:
+    def __init__(self, val):
+        self.val = val
+        self.left, self.right = None, None
+"""
+class Solution:
+    """
+    @param root: The root of binary tree.
+    @return: An integer
+    """ 
+    def maxDepth(self, root):
+        # write your code here
+        if root is None:
+            return 0
+        return max(self.maxDepth(root.left), self.maxDepth(root.right))+1
